84bf17a7fa7bf695e20d1184a45d4c6856396a55
Manish <manishlokhande96@gmail.com>
2019-10-26 00:27:13 -0700

    admin changes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const { ensureAuthenticated }= require('./../config/auth');
 
 //to get data in table
 const Files = require('./../models/files');
+const User = require('./../models/user');
 
 //Welcome Page
 router.get('/',(req,res)=>res.render('welcome'));
@@ -15,20 +16,27 @@ router.get('/dashboard',ensureAuthenticated,(req,res)=>{
 
     if(req.user.name == 'admin'){
 
-        Files.find({ },(err, data) => {
-            //console.log(data);
-            res.render('dashboard',{
-                user: user,
-                data: data
+        User.find({ level: { $ne: 'A' }},(err, output) => {
+            // console.log(output);
+            Files.find({},(err, data) => {
+                //console.log(data);
+                res.render('dashboard',{
+                    user: user,
+                    data: data,
+                    logins: output
+                })
             })
         })
+
+      
     }
     else{
         Files.find({ email : req.user.email },(err, data) => {
             //console.log(data);
             res.render('dashboard',{
                 user: user,
-                data: data
+                data: data,
+                logins: {}
             })
         })
     }
